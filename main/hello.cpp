549bf7e90fb44d2a4a8f086c40ec8d71d3a631d0
Arushi Upmanyu <106816650+arushiupmanyu@users.noreply.github.com>
2024-03-11 00:08:25 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World, this is PES2UG21CS906\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins cutie\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
